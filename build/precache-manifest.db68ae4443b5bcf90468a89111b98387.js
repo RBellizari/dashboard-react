5729cebd79f3c4fc913e020f637dd5fa0faf0bb9
Robson Bellizari <robsonbellizari@iMac-de-Robson.local>
2020-05-06 06:53:02 -0300

    teste

---
+++ build/precache-manifest.db68ae4443b5bcf90468a89111b98387.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6517596aef5a53104c06a5c6ce71b1a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e40c7bc59527f9d9325e",
+    "url": "/static/js/2.e43d55b4.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/static/js/2.e43d55b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "744c2d5f044505bed771",
+    "url": "/static/js/main.675874b5.chunk.js"
+  },
+  {
+    "revision": "285ff3dd32679ef7dbba",
+    "url": "/static/js/runtime-main.4a2d6595.js"
+  },
+  {
+    "revision": "6747665ce25cde9619512b3e263b4788",
+    "url": "/static/media/Roboto.6747665c.woff"
+  },
+  {
+    "revision": "a28925209cdc39a408c3d7880c7e5938",
+    "url": "/static/media/Roboto.a2892520.woff2"
+  }
+]);
