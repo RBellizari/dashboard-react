60651e029887131366c6c1dd94509a63ae44d039
Robson Bellizari <robsonbellizari@iMac-de-Robson.local>
2020-05-06 09:39:43 -0300

    Updates

---
+++ precache-manifest.5dec2260ac6abbc38c74e9f8d990ee48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb699f49ab088c08e4822fe2e3f82e25",
+    "url": "/dashboard-react/index.html"
+  },
+  {
+    "revision": "d239e258feea9e7eebbd",
+    "url": "/dashboard-react/static/js/2.8c424f65.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/dashboard-react/static/js/2.8c424f65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3172018c0e8c6c2484c",
+    "url": "/dashboard-react/static/js/main.835a36ea.chunk.js"
+  },
+  {
+    "revision": "f0ee171ffc3c06f31d76",
+    "url": "/dashboard-react/static/js/runtime-main.fe52b526.js"
+  },
+  {
+    "revision": "6747665ce25cde9619512b3e263b4788",
+    "url": "/dashboard-react/static/media/Roboto.6747665c.woff"
+  },
+  {
+    "revision": "a28925209cdc39a408c3d7880c7e5938",
+    "url": "/dashboard-react/static/media/Roboto.a2892520.woff2"
+  }
+]);
